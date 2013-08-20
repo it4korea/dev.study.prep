a5df0a9278b48986bfccf5bfad89dbc83da2f27c
Jaejin Kim <nije1983@gmail.com>
2013-08-20 13:29:00 +0900

    mission 7 added

---
+++ mission7/nije/blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '772dd4478c55244a4c5aba23a42ffdb1f4be1eab1d78b12978611ba44a01b982a975084ef8aa28d07a416c01fb1f69f17774392604402246f2aaccbe78f49b29'
